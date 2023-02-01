520610ff03ea0b0d4b51facb02641b6255daf96d
GreezllePL <greezlle@outlook.com>
2023-02-01 20:01:49 +0100

    Upload1

---
+++ data.js
@@ -0,0 +1,150 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-tour2ok",
+      "name": "Tour2ok",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.2698547535775937,
+        "pitch": 0.4032309902658824,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.03284784372900518,
+          "pitch": 0.3713412582077673,
+          "rotation": 3.141592653589793,
+          "target": "1-tour1ok"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-tour1ok",
+      "name": "Tour1ok",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -2.8133641158049993,
+        "pitch": 0.48605588063352023,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.987614717099696,
+          "pitch": 0.49808327160218546,
+          "rotation": 3.141592653589793,
+          "target": "2-tour3ok"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-tour3ok",
+      "name": "Tour3ok",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.2552945004728464,
+        "pitch": 0.6124528580505455,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0915011804767865,
+          "pitch": 0.6503318889096832,
+          "rotation": 3.141592653589793,
+          "target": "1-tour1ok"
+        },
+        {
+          "yaw": 0.9407279189419295,
+          "pitch": 0.2266885497085802,
+          "rotation": 3.141592653589793,
+          "target": "0-tour2ok"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.7420868069516136,
+          "pitch": 0.9626652141514409,
+          "title": "GEO4MAT",
+          "text": "Test"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
